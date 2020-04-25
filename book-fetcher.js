0f2899fde23bf488568ecdd1d3bdb6f0a373008e
milkywaynian <marina.faraji1@gmail.com>
2020-04-25 21:24:08 +0100

    install express

--- book-fetcher.js
+++ book-fetcher.js
@@ -4,27 +4,32 @@ console.log("Script run");
 const books = require('google-books-search');
 // request a book title and get a jSon response from google books API 
 
-// fetch('https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes')
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
+fetch('https://www.googleapis.com/books/v1/volumes?q=search+terms')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
 
-const options = {
-    key: process.env.KEY
-};
+// const options = {
+//     key: process.env.KEY 
+// };
 
+// const api_key = process.env.API_KEY;
 
+// const options = {
+//     key: api_key
+// };
 
-books.search('Professional JavaScript for Web Developers', options, function(error, results) {
-    if ( ! error ) {
-        console.log(results);
-    } else {
-        console.log(error);
-    }
-});
+//How to convert to a promise?
+// books.search('Professional JavaScript for Web Developers', options,  function(error, results) {
+//     if ( ! error ) {
+//         console.log(results);
+//     } else {
+//         console.log(error);
+//     }
+// });
 
 
 
